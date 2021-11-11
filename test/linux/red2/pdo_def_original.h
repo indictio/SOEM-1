b286999d347f427414a8c865bc5a17519a05d34a
Indictio <office@indictio.ro>
2021-11-12 01:03:05 +0200

    last changes

--- test/linux/red2/pdo_def_original.h
+++ test/linux/red2/pdo_def_original.h
@@ -9,19 +9,18 @@
 typedef struct PACKED
 {
     uint16 control_word_6040;
+    int8 modes_of_operation_6060;
     int32 target_position_607a;
-    int32 target_velocity_60ff;
-    int16 target_torque_6071;
 } out_IS620N_t;
 
 typedef struct PACKED
 {
     uint16 status_word_6041;
+    int8 modes_of_operation_display_6061;
     uint16 error_code_603f;
     int32 position_actual_value_6064;
     int32 velocity_actual_value_606c;
     int16 torque_actual_value_6077;
-    int8 modes_of_operation_display_6061;
 } in_IS620N_t;
 
 typedef struct
